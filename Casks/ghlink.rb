8976c995fac4d949598905c66e8729375668811a
Mason Lee <liwangmj@gmail.com>
2026-08-25 13:50:20 +0800

    fix: cask version 0.4.14 -> 0.4.15 (match pkg sha256)

--- Casks/ghlink.rb
+++ Casks/ghlink.rb
@@ -1,5 +1,5 @@
 cask "ghlink" do
-  version "0.4.14"
+  version "0.4.15"
   sha256 "165f97fe401ebd974adb26e87e6a599d5e3cbe20b34069a63cda945c26234dff"
 
   url "https://github.com/liwmj/ghlink/releases/download/v#{version}/ghlink-#{version}.pkg",
